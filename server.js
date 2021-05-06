afa56cba04eb9e388231cf3362f6f394c5ede189
johaol16 <johanna.n.olsson@hotmail.com>
2021-05-06 16:25:35 +0200

    final

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const { request, response } = require("express")
 const express = require("express")
 const lowdb = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-
 const { nanoid } = require('nanoid')
 
 const adapterMenu = new FileSync('menu.json')
@@ -41,13 +40,9 @@ app.get('/api/coffee', (request, response) => {
 
  app.post('/api/account', (request, response) => {
   const account = request.body
-  /* console.log('Konto att lägga till:', account) */
 
-    const usernameExists = databaseAccounts.get('accounts').find({ username: account.username }).value()
+  const usernameExists = databaseAccounts.get('accounts').find({ username: account.username }).value()
   const emailExists = databaseAccounts.get('accounts').find({ email: account.email }).value()
-
-  console.log('usernameExists:', usernameExists)
-  console.log('emailExists', emailExists)
 
   const result = {
     success: false,
@@ -78,12 +73,68 @@ app.post('/api/order', (request, response) => {
   order.id = nanoid()
   order.eta =  10 + Math.floor(Math.random() * 20)
 
+  let today = new Date();
+  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let orderTime = date+' '+time; 
+  order.orderTime = orderTime
+
   const account = databaseAccounts.get('accounts').find({ username: order.username }).value()
+
 if (account) {
   databaseAccounts.get('orders').push(order).write()
 }
+
   response.json(order)
+
 })
+
+
+
+app.get('/api/order/:id', (request, response) => {
+  const order = request.params.id
+
+  const user = databaseAccounts.get('orders').filter({ username: order }).value()
+
+
+const eta = user.map(function (time) {
+  return time.eta
+})
+console.log(eta)
+
+
+  let today = new Date();
+  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let currentTime = date+' '+time; 
+  console.log(currentTime)
+
+
+const orderTime = user.map(function (time) {
+  time.orderTime = new Date(time.orderTime);
+  return time;
+});
+
+
+for(let i = 0; i < orderTime.length; i++) {
+  let diff = Math.abs(today - orderTime[i].orderTime);
+  let minutes = Math.floor((diff/1000)/60)
+
+ 
+    if (minutes >= eta[i]) {
+    console.log('done')
+  orderTime[i].orderDone = true
+  }
+  if (minutes < eta[i]) {
+    console.log('not done')
+    orderTime[i].orderDone = false
+  }
+}
+
+  response.json(user)
+
+}); 
+
 
 
 app.listen(8000, () => {
